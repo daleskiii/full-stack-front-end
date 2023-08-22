6822d574cc236b7913d294605c427daa02aa32bd
daleskiii <aun2793@gmail.com>
2023-08-22 16:43:11 -0400

    new commit for productio

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+
 import "./Products.css";
 import { useCart } from "../Context/CartContext";
 import { useAuth } from "../Context/AuthContext";
