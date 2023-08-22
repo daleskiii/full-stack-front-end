08867e94c9bf85514b97e64b099c6ad2df506875
daleskiii <aun2793@gmail.com>
2023-08-22 18:06:14 -0400

    new commit

--- src/components/Oders/Orders.js
+++ src/components/Oders/Orders.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+
 import "./Orders.css";
 import { getOrderByUser, deleteOder } from "../API/API";
 import { useAuth } from "../Context/AuthContext";
